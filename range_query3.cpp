cbaed63b76c9a443f038792a7ff44044a07d10c5
baquyptit2001 <67578269+baquyptit2001@users.noreply.github.com>
2020-09-17 22:11:53 +0700

    Create range_query3.cpp

---
+++ range_query3.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int nt(int n){
+    if(n<2)
+        return 0;
+    for(int i=2;i<=sqrt(n);i++)
+        if(n%i==0)
+            return 0;
+    return 1;
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int l, r;
+        cin>>l>>r;
+        int dem=0;
+        for(int i=l;i<=r;i++)
+            if(nt(i))
+                dem++;
+        cout<<dem<<endl;
+    }
+    return 0;
+}
